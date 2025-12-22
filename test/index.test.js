ac16b1249d6cad1e44a4194e5e5d2014cbb5498d
qntm <qntm@users.noreply.github.com>
2025-12-22 00:45:32 +0000

    Update index.test.js (#22)

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,8 @@
 /* eslint-env mocha */
 
-import assert from 'assert'
-import fs from 'fs'
+import assert from 'node:assert/strict'
+import fs from 'node:fs'
+
 import { encode, decode } from '../src/index.js'
 
 const uint8ArrayFromHex = hex => Uint8Array.from(hex.match(/../g).map(xx => Number.parseInt(xx, 16)))
@@ -12,22 +13,22 @@ const md5 = Uint8Array.from([0xd4, 0x1d, 0x8c, 0xd9, 0x8f, 0x00, 0xb2, 0x04, 0xe
 describe('base65537', () => {
   describe('encode', () => {
     it('works', () => {
-      assert.deepStrictEqual(encode(Uint8Array.from([])), '')
-      assert.deepStrictEqual(encode(Uint8Array.from([0])), '㐀')
+      assert.deepEqual(encode(Uint8Array.from([])), '')
+      assert.deepEqual(encode(Uint8Array.from([0])), '㐀')
     })
   })
 
   describe('decode', () => {
     it('works', () => {
-      assert.deepStrictEqual(decode(''), Uint8Array.from([]))
-      assert.deepStrictEqual(decode('㐀'), Uint8Array.from([0]))
-      assert.deepStrictEqual(decode('💩'), Uint8Array.from([255, 0]))
-      assert.deepStrictEqual(decode('💩💩'), Uint8Array.from([255, 1, 255, 1]))
-      assert.deepStrictEqual(
+      assert.deepEqual(decode(''), Uint8Array.from([]))
+      assert.deepEqual(decode('㐀'), Uint8Array.from([0]))
+      assert.deepEqual(decode('💩'), Uint8Array.from([255, 0]))
+      assert.deepEqual(decode('💩💩'), Uint8Array.from([255, 1, 255, 1]))
+      assert.deepEqual(
         decode('💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩'),
         uint8ArrayFromHex('ff15fffc05ea219682718955bcab7a5577367415a0a1a09e740d772c7a4cbca589528270219605e9fffbff15')
       )
-      assert.deepStrictEqual(
+      assert.deepEqual(
         decode('💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩'),
         uint8ArrayFromHex('ff8c2593fa4dd670941e2619c02e0c2e1ac551a18ab8d1ae7ca7ede9f3b6aac3667d3ca3f1d22db07ed364308d64c673e9137ea4c4f04be24148e7ccc68a4310dbddaa20c30d7217f1816713fede93255c5116f4fb4f219c3b7d632057d93c9d561a982d0aa80a1caa98d0f5057edf0f37f3e49d997ded1814d1b43a42685c1e87f13269fd1d27f65130c13cb064074c5dff9cbdfa6f6d037a1d4672301577791767c8eae198d9650e11f3cd076b3d4119ca05d68d2b9defb9e9fd3425f74a3a25eecd38a4b4af1b35f4613c15793ecf7284426e3dba861574c3fe1719247f40281d918a36c81b18bd826e553cf957df3a4cb5e5ac59bb896b13ce7dc6da2e92e8296086d14abd73d9dde17f3d374cd6bcb0f95d258cff8b')
       )
@@ -40,8 +41,8 @@ describe('base65537', () => {
 
   describe('demo', () => {
     it('works', () => {
-      assert.deepStrictEqual(encode(md5), '䯕傺檸𥴏髌𡎱𦸜💩')
-      assert.deepStrictEqual(decode('䯕傺檸𥴏髌𡎱𦸜💩'), md5)
+      assert.deepEqual(encode(md5), '䯕傺檸𥴏髌𡎱𦸜💩')
+      assert.deepEqual(decode('䯕傺檸𥴏髌𡎱𦸜💩'), md5)
     })
   })
 
@@ -56,7 +57,7 @@ describe('base65537', () => {
     ]
     uint8Arrays.forEach((uint8Array, i) => {
       it(`case #${i}`, () => {
-        assert.deepStrictEqual(decode(encode(uint8Array)), uint8Array)
+        assert.deepEqual(decode(encode(uint8Array)), uint8Array)
       })
     })
   })
